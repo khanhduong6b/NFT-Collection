5e6ea2e929e1b8df3f17cb1c768b75a44ed8d059
Khanh Duong Ho <duonghk.tesse@gmail.com>
2023-04-11 17:22:59 +0700

    Init

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x376ecE39728469144c53229fDe1258D208a25216";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-kd0107.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
